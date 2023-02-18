898e77bdc23de68f0638522fd4a72018c38d900a
Uzerkhan786 <uzer.khan2020@vitbhopal.ac.in>
2023-02-18 22:25:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+
+
+setTimeout(() => {
+   document.getElementById("1").innerHTML="Mern"; 
+}, 2300);
+
+setTimeout(() => {
+    document.getElementById("2").innerHTML=" Stack"; 
+ }, 2700);
+
+ setTimeout(() => {
+    document.getElementById("3").innerHTML="Developer"; 
+ },3000);
+
+ setTimeout(() => {
+    document.getElementById("4").innerHTML="UZER"; 
+ },1500 );
+
+ setTimeout(() => {
+    document.getElementById("5").innerHTML="Khan"; 
+ }, 1900);
